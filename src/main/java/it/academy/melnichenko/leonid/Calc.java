19ecf2d6206b9248abece89164f1ed13854f5356
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-05-02 20:03:16 +0300

    add multiplication

--- src/main/java/it/academy/melnichenko/leonid/Calc.java
+++ src/main/java/it/academy/melnichenko/leonid/Calc.java
@@ -8,10 +8,10 @@ public class Calc {
     public static int subtract(int a, int b) {
         return a - b;
     }
-//
-//    public static int multiplication(int a, int b){
-//        return a * b;
-//    }
+
+    public static int multiplication(int a, int b){
+        return a * b;
+    }
 //    public static int division(int a, int b){
 //        return a / b;
 //    }
