b75b6a1b0a8089b417e268e5e6eb42f8f8d6cc63
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-05-02 20:03:50 +0300

    add division

--- src/main/java/it/academy/melnichenko/leonid/Calc.java
+++ src/main/java/it/academy/melnichenko/leonid/Calc.java
@@ -12,7 +12,7 @@ public class Calc {
     public static int multiplication(int a, int b){
         return a * b;
     }
-//    public static int division(int a, int b){
-//        return a / b;
-//    }
+    public static int division(int a, int b){
+        return a / b;
+    }
 }
