3142bf0cba32a86e9c202114822d873b8ef1eb5c
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-05-02 20:01:48 +0300

    add summa

--- src/main/java/it/academy/melnichenko/leonid/Calc.java
+++ src/main/java/it/academy/melnichenko/leonid/Calc.java
@@ -5,14 +5,14 @@ public class Calc {
         return a + b;
     }
 
-    public static int subtract(int a, int b) {
-        return a - b;
-    }
-
-    public static int multiplication(int a, int b){
-        return a * b;
-    }
-    public static int division(int a, int b){
-        return a / b;
-    }
+//    public static int subtract(int a, int b) {
+//        return a - b;
+//    }
+//
+//    public static int multiplication(int a, int b){
+//        return a * b;
+//    }
+//    public static int division(int a, int b){
+//        return a / b;
+//    }
 }
