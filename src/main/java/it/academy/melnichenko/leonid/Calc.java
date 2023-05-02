6846639b957b70716d1906721cd949caed24dfb2
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-05-02 20:02:30 +0300

    add subtract

--- src/main/java/it/academy/melnichenko/leonid/Calc.java
+++ src/main/java/it/academy/melnichenko/leonid/Calc.java
@@ -5,9 +5,9 @@ public class Calc {
         return a + b;
     }
 
-//    public static int subtract(int a, int b) {
-//        return a - b;
-//    }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
 //
 //    public static int multiplication(int a, int b){
 //        return a * b;
